5ef4ee45c9616ef5655ed2bde69ea83fe772c15e
Jinny <ppwm111@naver.com>
2023-02-15 18:41:26 +0900

    백엔드 만드는 중

--- back/models/comment.js
+++ back/models/comment.js
@@ -1,27 +1,19 @@
-const DataTypes = require('sequelize');
-const { Model } = DataTypes;
-
-module.exports = class Comment extends Model {
-  static init(sequelize) {
-    return super.init({
+module.exports = (sequelize, DataTypes) => {
+  const Comment = sequelize.define(
+    'Comment',
+    {
+      // MySQL에는 Comments 테이블 생성
       // id가 기본적으로 들어있다.
       content: {
         type: DataTypes.TEXT,
         allowNull: false,
       },
-      // UserId: 1
-      // PostId: 3
-    }, {
-      modelName: 'Comment',
-      tableName: 'comments',
+    },
+    {
       charset: 'utf8mb4',
       collate: 'utf8mb4_general_ci', // 이모티콘 저장
-      sequelize,
-    });
-  }
-
-  static associate(db) {
-    db.Comment.belongsTo(db.User);
-    db.Comment.belongsTo(db.Post);
-  }
+    },
+  );
+  Comment.associate = (db) => {};
+  return Comment;
 };
